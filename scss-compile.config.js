a920998f222914d87b17fb0bc1298efb13febcdd
Saltssaumure <themes@saltssaumure.net>
2023-08-25 02:54:06 +0100

    Convert to scss

---
+++ scss-compile.config.js
@@ -0,0 +1,9 @@
+/** @type {import('bd-scss/lib/config').Config} */
+export default {
+    meta: {
+        name: "Espresso" /* The official name of your theme. */,
+        scss: "espresso" /* The name of your base scss file. */,
+        repo: "Espresso" /* The name of your theme repository. */,
+        version: "dev" /* The version of your theme. */
+    }
+};
